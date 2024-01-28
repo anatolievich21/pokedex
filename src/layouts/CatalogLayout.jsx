5e6fded619925c38137724788568c1c97a84c44f
anatolievich21 <pastushenkovladislav21@gmail.com>
2024-01-28 20:55:08 +0200

    add pokedex
    start proj

---
+++ src/layouts/CatalogLayout.jsx
@@ -0,0 +1,15 @@
+import {PokemonList, LoadMoreButton, ScrollToTopButton} from "../components/index.js";
+import {Outlet} from "react-router-dom";
+
+export const CatalogLayout = () => {
+    return <div className='container-content'>
+        <div className='left-section'>
+            <PokemonList />
+            <LoadMoreButton />
+        </div>
+        <div className='right-section'>
+            <Outlet />
+        </div>
+        <ScrollToTopButton />
+    </div>
+}
